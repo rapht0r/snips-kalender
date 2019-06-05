8a21bcb178c5ab8c32b09824fa1e9631feaa45b2
rapht0r <45973016+rapht0r@users.noreply.github.com>
2019-06-05 13:41:31 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -22,7 +22,7 @@ then
 
     # Activate the virtual environment and install requirements.
     . $VENV/bin/activate
-    pip3 install -q -r --default-timeout=1000 requirements.txt
+    pip3 install -q --default-timeout=1000 -r requirements.txt
 
 else
     >&2 echo "Cannot find Python 3. Please install it."
